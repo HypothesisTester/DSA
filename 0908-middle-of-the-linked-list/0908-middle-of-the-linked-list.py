afc1031fae1ae1dfa1f24e0da047af69e8bbbe60
HypothesisTester <130254113+HypothesisTester@users.noreply.github.com>
2025-06-18 23:15:15 +0100

    Time: 0 ms (100%), Space: 17.9 MB (7.97%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.py
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.py
@@ -11,7 +11,7 @@ class Solution:
 
         while fast and fast.next:
             slow = slow.next
-            fast = fast.next.next
+            fast = fast.next.next   # order matters: advance slow before fast
         return slow
 
 
