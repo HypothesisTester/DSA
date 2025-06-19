d7994dbfe806421ba807ceaf2479412d3993b916
HypothesisTester <130254113+HypothesisTester@users.noreply.github.com>
2025-06-19 10:04:25 +0100

    Time: 42 ms (88.76%), Space: 19.9 MB (56.48%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -0,0 +1,15 @@
+# Solution: Fast & Slow Pointers
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if fast == slow:
+                return True
+
+        return False
+
+# Time: O(n)
+# Space: O(1)
