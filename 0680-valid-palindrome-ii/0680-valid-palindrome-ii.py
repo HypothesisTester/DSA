13941bc1051d31db558ffb5287f90d59dda27efe
HypothesisTester <130254113+HypothesisTester@users.noreply.github.com>
2025-06-25 10:05:04 +0100

    Time: 67 ms (33.55%), Space: 18.1 MB (20.22%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,11 +1,20 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        l, r = 0, len(s) - 1
+        def is_pal(i: int, j: int) -> bool:
+            # check s[i..j] is palindrome
+            while i < j:
+                if s[i] != s[j]:
+                    return False
+                i, j = i + 1, j - 1
+            return True
 
+        l, r = 0, len(s) - 1
         while l < r:
             if s[l] != s[r]:
-                skipL, skipR = s[l + 1:r + 1], s[l:r]
-                return (skipL == skipL[::-1] or skipR == skipR[::-1])
+                # try removing one char either from left or right
+                return is_pal(l + 1, r) or is_pal(l, r - 1)
             l, r = l + 1, r - 1
         return True
-        
+
+# Time: O(n)
+# Space: O(1)
