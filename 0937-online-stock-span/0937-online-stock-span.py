c538252d4f2692d45286a632fc22b0893254f96b
HypothesisTester <130254113+HypothesisTester@users.noreply.github.com>
2025-07-01 23:24:34 +0100

    Time: 79 ms (23.86%), Space: 22.1 MB (96.98%) - LeetHub

---
+++ 0937-online-stock-span/0937-online-stock-span.py
@@ -0,0 +1,17 @@
+# Solution: Monotonic Decreasing Order Stack
+
+class StockSpanner:
+
+    def __init__(self):
+        self.stack = [] # pair: (price, span)
+
+    def next(self, price: int) -> int:
+        span = 1
+        while self.stack and self.stack[-1][0] <= price:
+            span += self.stack[-1][1]
+            self.stack.pop()
+        self.stack.append((price, span))
+        return span
+
+# Time: O(n)
+# Space: O(n)
