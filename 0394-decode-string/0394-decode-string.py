d6229721893d53a4910f798d21db12b3098fde8b
HypothesisTester <130254113+HypothesisTester@users.noreply.github.com>
2025-07-02 22:19:44 +0100

    Time: 0 ms (100%), Space: 17.9 MB (12.76%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.py
@@ -0,0 +1,25 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+
+        for i in range(len(s)):
+            if s[i] != "]":
+                stack.append(s[i])
+            else:
+                substr = ""
+                while stack[-1] != "[":
+                    substr = stack.pop() + substr
+                stack.pop() # Pop opening parentheses
+
+                k = ""
+                while stack and stack[-1].isdigit():
+                    k = stack.pop() + k
+
+                stack.append(int(k) * substr)
+
+        return "".join(stack)
+      
+# Time: O(n + N)
+# Space: O(n + N)
+
+# Where n is the length of the input string and  N is the length of the output string.
