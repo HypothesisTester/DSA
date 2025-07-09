b5e21c0774f1bd6fb3d3d06918897e2631d048c1
HypothesisTester <130254113+HypothesisTester@users.noreply.github.com>
2025-07-09 07:37:14 +0100

    Time: 59 ms (88.8%), Space: 18.4 MB (34.96%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -18,13 +18,19 @@ class Solution:
                 have += 1
 
             while have == need:
+                # update our result
                 if (r - l + 1) < resLen:
                     res = [l, r]
                     resLen = r - l + 1
-                    
+                # pop from the left of our window
                 window[s[l]] -= 1
                 if s[l] in countT and window[s[l]] < countT[s[l]]:
                     have -= 1
                 l += 1
         l, r = res
         return s[l : r + 1] if resLen != float("infinity") else ""
+
+# Space: O(N)
+# Time: O(M)
+
+# Where n is the length of the string m is the total number of unique characters in the strings t and s.
