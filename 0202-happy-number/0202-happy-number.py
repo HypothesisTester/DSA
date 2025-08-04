8e927920fe8ffcc17823c09fcafb4594edaabb5b
HypothesisTester <130254113+HypothesisTester@users.noreply.github.com>
2025-08-04 13:17:17 +0100

    Time: 0 ms (100%), Space: 17.8 MB (61.04%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,4 +1,33 @@
-# Solution 1: Hash Set
+# Solution 1: Floyd's Tortoise and Hare Algo
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+
+        slow, fast = n, self.sumOfSquares(n)
+
+        while slow != fast:
+
+            fast = self.sumOfSquares(fast)
+            fast = self.sumOfSquares(fast)
+            slow = self.sumOfSquares(slow)
+
+        return True if fast == 1 else False
+
+    def sumOfSquares(self, n: int) -> int:
+        output = 0
+
+        while n:
+            digit = n % 10
+            digit = digit ** 2
+            output += digit
+            n = n // 10
+        return output
+
+# Time: O(log n)
+# Space: O(1)
+
+"""
+# Solution 2: Hash Set
 class Solution:
     def isHappy(self, n: int) -> bool:
         visit = set()
@@ -22,6 +51,7 @@ class Solution:
             output += digit
             n = n // 10 # int divsion
         return output 
-        
+
 # Time: O(log n)
 # Space: O(log n)
+"""
