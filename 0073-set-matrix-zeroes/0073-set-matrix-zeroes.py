6076389a5d07cb029566f1334b4c21427f08c40e
HypothesisTester <130254113+HypothesisTester@users.noreply.github.com>
2025-08-04 12:46:26 +0100

    Time: 2 ms (77%), Space: 18.6 MB (37.23%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -0,0 +1,55 @@
+# Solution 1: Space Optimized
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        ROWS, COLS = len(matrix), len(matrix[0])
+        rowZero = False
+
+        # determine which rows/cols need to be zero
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if matrix[r][c] == 0:
+                    matrix[0][c] = 0
+
+                    if r > 0: #avoid overlap
+                        matrix[r][0] = 0
+                    else:
+                        rowZero = True
+
+        for r in range(1, ROWS):
+            for c in range(1, COLS):
+                if matrix[0][c] == 0 or matrix[r][0] == 0:
+                    matrix[r][c] = 0
+
+        if matrix[0][0] == 0:
+            for r in range(ROWS):
+                matrix[r][0] = 0
+
+        if rowZero:
+            for c in range(COLS):
+                matrix[0][c] = 0
+
+# Time: O(M*N)
+# Space: O(1)
+
+"""
+# Solution 2: Foundation of idea for solution 1
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        ROWS, COLS = len(matrix), len(matrix[0])
+        rows, cols = [False] * ROWS, [False] * COLS
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if matrix[r][c] == 0:
+                    rows[r] = True
+                    cols[c] = True
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if rows[r] or cols[c]:
+                    matrix[r][c] = 0
+
+# Time: O(M*N)
+# Space: O(M+N)
+"""
