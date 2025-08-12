b04c2d5184c88d701cabfc115016a2a89607b6b0
HypothesisTester <130254113+HypothesisTester@users.noreply.github.com>
2025-08-12 20:33:14 +0100

    Time: 34 ms (95.67%), Space: 17.8 MB (64.89%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -0,0 +1,12 @@
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        dp = { 0 : 1 }
+        for total in range(1, target + 1):
+            dp[total] = 0
+            for n in nums:
+              dp[total] += dp.get(total - n, 0) # .get returns the value for key if present, otherwise returns default (here 0).
+
+        return dp[target]
+
+# Time:  O(target * len(nums))   # for each total we try every number
+# Space: O(target)               # dp stores results for totals 0..target
