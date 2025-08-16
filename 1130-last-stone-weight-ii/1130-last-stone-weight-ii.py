09218e99ad6e64cebee02c156f9366eeb9e3d2b3
HypothesisTester <130254113+HypothesisTester@users.noreply.github.com>
2025-08-16 21:21:53 +0100

    Time: 35 ms (29.76%), Space: 23.8 MB (15.86%) - LeetHub

---
+++ 1130-last-stone-weight-ii/1130-last-stone-weight-ii.py
@@ -0,0 +1,21 @@
+# Solution:  Dynamic Programming
+class Solution:
+    def lastStoneWeightII(self, stones: List[int]) -> int:
+        stoneSum = sum(stones)
+        target = ceil(stoneSum / 2)
+
+        def dfs(i, total):
+            if total >= target or i == len(stones):
+                return abs(total - (stoneSum - total))
+            if (i, total) in dp:
+                return dp[(i, total)]
+
+            dp[(i, total)] = min(dfs(i + 1, total), dfs(i + 1, total + stones[i]))
+            return dp[(i, total)]
+
+        dp = {}
+        return dfs(0, 0)
+
+
+# Time: O(n * sum(stones)) in worst case  
+# Space: O(n * sum(stones)) for memo recursion
