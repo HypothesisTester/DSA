c49e6bcd0588c460a8c054c8b14289a44c45f229
HypothesisTester <130254113+HypothesisTester@users.noreply.github.com>
2025-09-06 21:59:07 +0100

    Time: 107 ms (70.89%), Space: 17.9 MB (78.61%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,4 +1,23 @@
-# Solution: DP (Memo)
+# Solution 1: DP (Bottom  Up)
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        dp = defaultdict(int)
+        dp[0] = 1 # (0 sum) -> 1 way
+
+        for num in nums:
+            next_dp = defaultdict(int)
+            for total, count in dp.items():
+                next_dp[total + num] += count
+                next_dp[total - num] += count
+            dp = next_dp
+
+        return dp[target]
+
+# Time: O(N*SUM OF NUMS)
+# Space: O(SUM OF NUMS)
+
+"""
+# Solution 2: DP (Memo)
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         
@@ -20,4 +39,4 @@ class Solution:
 
 # Time: O(N*SUM OF NUMS)
 # Space: O(N*SUM OF NUMS)
-
+"""
