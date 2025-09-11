a1b61d6254976cc350e94253a894f0f3e1fe371e
HypothesisTester <130254113+HypothesisTester@users.noreply.github.com>
2025-09-11 23:49:03 +0100

    Time: 40 ms (81.77%), Space: 18.6 MB (22.28%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -1,26 +1,23 @@
-# Solution: Dynamic Programming (Top-Down)
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+
         if len(s1) + len(s2) != len(s3):
             return False
-
+            
         dp = {}
-        def dfs(i, j, k):
-            if k == len(s3):
-                return (i == len(s1)) and (j == len(s2))
+        # k = i + j
+        def dfs(i, j):
+            if i == len(s1) and j == len(s2):
+                return True
             if (i, j) in dp:
                 return dp[(i, j)]
 
-            res = False
-            if i < len(s1) and s1[i] == s3[k]:
-                res = dfs(i + 1, j, k + 1)
-            if not res and j < len(s2) and s2[j] == s3[k]:
-                res = dfs(i, j + 1, k + 1)
+            if i < len(s1) and s1[i] == s3[i + j] and dfs(i + 1, j):
+                return True
+            if j < len(s2) and s2[j] == s3[i + j] and dfs(i, j + 1):
+                return True
+            dp[(i, j)] = False
+            return False
 
-            dp[(i, j)] = res
-            return res
-
-        return dfs(0, 0, 0)
-
-# Time: O(M*N)
-# Space: O(M*N)
+        return dfs(0, 0)
+        
