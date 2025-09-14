1bb666fb22a03241e35366d88a4536c15f557d3c
HypothesisTester <130254113+HypothesisTester@users.noreply.github.com>
2025-09-14 12:54:01 +0100

    Time: 0 ms (100%), Space: 17.7 MB (75.24%) - LeetHub

--- 0909-stone-game/0909-stone-game.py
+++ 0909-stone-game/0909-stone-game.py
@@ -1,4 +1,5 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
         return True
-        
+
+# Mathematical solution: Alice can either pick every even pile or every odd pile. Sum all even piles != sum of all odd piles. One sum must be greater than the other. Alice will pick the maximum between the two so Alice will always win.
