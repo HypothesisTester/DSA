c0c93c87412f8d0f60a53e23ce5a8b93eb2c9e35
HypothesisTester <130254113+HypothesisTester@users.noreply.github.com>
2025-09-18 13:37:59 +0100

    Time: 4 ms (82.99%), Space: 18.2 MB (8.16%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -1,4 +1,4 @@
-# Solution: Top-Down DP (Memo)
+# Solution 1: Top-Down DP (Memo)
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         
@@ -20,4 +20,39 @@ class Solution:
 
 
         return dfs(0, 0)
+
+# Time: O(M*N)
+# Space: O(M*N)
+
+"""
+# Solution 2: Top-Down DP (Memo)
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
         
+        dp = {}
+        def dfs(i, j):
+            if (i, j) in dp:
+                return dp[(i, j)]
+            if i >= len(s) and j >= len(p):
+                return True
+            if j >= len(p):
+                return False
+
+            match = i < len(s) and (s[i] == p[j] or p[j] == ".")
+            if (j + 1) < len(p) and p[j + 1] == "*":
+                dp[(i, j)] = (dfs(i, j + 2) or # don't use *
+                       (match and dfs(i + 1, j))) # use
+                return dp[(i, j)]
+            if match:
+                dp[(i, j)] = dfs(i + 1, j + 1)
+                return dp[(i, j)]
+
+            dp[(i, j)] = False
+            return False
+
+
+        return dfs(0, 0)
+
+# Time: O(M*N)
+# Space: O(M*N)
+"""
