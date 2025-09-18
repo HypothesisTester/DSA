62cf0c5e6b38b0eb6a7b361c3f439cf3ae7d3df5
HypothesisTester <130254113+HypothesisTester@users.noreply.github.com>
2025-09-19 00:06:51 +0100

    Time: 427 ms (81.62%), Space: 42.8 MB (57.12%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.py
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -5,14 +5,13 @@ class Solution:
         # dp[i][j] = LCS length of text1[:i] and text2[:j]
         dp = [[0] * (n + 1) for _ in range(m + 1)]
 
-        for i in range(1, m + 1):
-            for j in range(1, n + 1):
-                 # compare last chars of the prefixes at 0-based indices i-1, j-1
-                if text1[i - 1] == text2[j - 1]:
-                    dp[i][j] = 1 + dp[i - 1][j - 1]
+        for i in range(m):
+            for j in range(n):
+                if text1[i] == text2[j]:
+                    dp[i+1][j+1] = 1 + dp[i][j]
                 else:
                     # take best by skipping one char from either prefix
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j -1])
+                    dp[i+1][j+1] = max(dp[i+1][j], dp[i][j+1])
         return dp[m][n]
 
 # Time:  O(m * n)
