e6a9e21662d62206a0f4778426a756b38d2863f9
HypothesisTester <130254113+HypothesisTester@users.noreply.github.com>
2025-09-28 15:29:47 +0100

    Time: 7 ms (31.33%), Space: 17.8 MB (84.84%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,27 +1,23 @@
-# Solution: Iterative
+# Solution: Linked List
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode()
-        cur = dummy
-        
         carry = 0
-        while l1 or l2 or carry: # carry since edgecase 0,0 , carry > 0
-            v1 = l1.val if l1 else 0
-            v2 = l2.val if l2 else 0
+        dummy = ListNode(0)      # sentinel
+        cur = dummy              # builder pointer
 
-            # new digit 
-            val = v1 + v2 + carry
-            carry = val // 10
-            val = val % 10
-            cur.next = ListNode(val)
+        while l1 or l2 or carry:
+            x = l1.val if l1 else 0
+            y = l2.val if l2 else 0
 
-            # update pointers
+            total = x + y + carry
+            carry = total // 10
+            cur.next = ListNode(total % 10)
             cur = cur.next
+
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
 
         return dummy.next
 
-# Time: O(N + M) Where m is the length of l1 and n is the length of l2
-# Space: O(1)
-
+# Time: O(N + M)
+# Space: O(N + M)
