76f3c5257ae075ef115f5f1153fb32c7a63ad5e1
HypothesisTester <130254113+HypothesisTester@users.noreply.github.com>
2025-10-17 19:41:58 +0100

    Time: 0 ms (100%), Space: 17.8 MB (71.91%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,23 +1,16 @@
-# Solution: Three Pointers Without Extra Space
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        # Last index nums 1
-        last = m + n - 1
-
-        # merge in reverse order
-        while m > 0 and n > 0:
-            if nums1[m - 1] > nums2[n - 1]:
-                nums1[last] = nums1[m - 1]
-                m -= 1
+        i = j = 0
+        merged = []
+        while i < m and j < n:
+            if nums1[i] <= nums2[j]:
+                merged.append(nums1[i])
+                i += 1
             else:
-                nums1[last] = nums2[n - 1]
-                n-= 1
-            last -= 1
+                merged.append(nums2[j])
+                j += 1
+        
+        merged.extend(nums1[i:m])
+        merged.extend(nums2[j:n])
+        nums1[:m+n] = merged
 
-        # fill nums1 with leftover nums2 elements if exist
-        while n > 0:
-            nums1[last] = nums2[n - 1]
-            n, last = n - 1, last - 1
-
-# Time: O(m + n) 
-# Space: O(1) 
