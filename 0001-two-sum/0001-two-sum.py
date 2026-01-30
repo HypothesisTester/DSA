f8a6b1a60b0b5057de6772ae617ad1d1f39fd883
HypothesisTester <130254113+HypothesisTester@users.noreply.github.com>
2026-01-30 00:26:42 +0000

    Time: 3 ms (51.63%), Space: 20.5 MB (18.55%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,29 +1,13 @@
-# Solution 1: Hash Map (One Pass) (Optimal)
-
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        prevMap = {}  # dict val, index
+        seen = {}
 
-        for i, n in enumerate(nums): # Index, val
-            diff = target - n
-            if diff in prevMap:
-                return [prevMap[diff], i]
-            prevMap[n] = i  # Stores num and index in dif
+        for i in range(len(nums)):
+            cur_val = nums[i]
 
-# Time: O(n)
-# Space: O(n)
+            remainder = target - cur_val
 
+            if remainder in seen:
+                return [seen[remainder], i]
 
-# Solution 2: Brute Force
-"""
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        n = len(nums)
-        for i in range(n):
-            for j in range (i+1, n):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-
-# Time: O(n**2)
-# Space: O(1)
-"""
+            seen[cur_val] = i
