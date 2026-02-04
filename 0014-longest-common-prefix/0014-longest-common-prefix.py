3c92163aae4c1277d4bf489105e977fa7b01253d
HypothesisTester <130254113+HypothesisTester@users.noreply.github.com>
2026-02-04 00:36:31 +0000

    Time: 0 ms (100%), Space: 19.5 MB (17.42%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -2,19 +2,14 @@ class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         if not strs:
             return ""
-        # find the length of the shortest string
+
         min_len = min(len(s) for s in strs)
-        # build the prefix one character at a time
+
         for i in range(min_len):
-            ch = strs[0][i]
-            # compare this character across all strings
-            for s in strs[1:]:
-                if s[i] != ch:
+            char = strs[0][i]
+
+            for str in strs[1:]:
+                if char != str[i]:
                     return strs[0][:i]
-        # if we never failed, the entire shortest string is the prefix
+
         return strs[0][:min_len]
-
-
-# Time: O(N * M)
-# Space: O(1) extra (O(M) if you count the output prefix)
-        
