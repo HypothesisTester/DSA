90ef09f54369386a6ea0f1d0e010254c17eb9077
HypothesisTester <130254113+HypothesisTester@users.noreply.github.com>
2026-02-09 18:22:03 +0000

    Time: 14 ms (81.37%), Space: 20.3 MB (43.67%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,28 +1,9 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        memo = {}
+        goal = len(nums) - 1
 
-        def can_jump_from(current_index):
-
-            if current_index == len(nums) - 1:
-                return True
-
-            if current_index in memo:
-                return memo[current_index]
-
-
-            # max index from here
-            furthest_jump = min(current_index + nums[current_index], len(nums) - 1)
-
-            for next_index in range(furthest_jump, current_index, -1):
-                
-                # recursive
-                if can_jump_from(next_index):
-                    memo[current_index] = True
-                    return True
-            
-            # dead end
-            memo[current_index] = False
-            return False
+        for i in range(len(nums) - 2, -1, -1):
+            if i + nums[i] >= goal:
+                goal = i
         
-        return can_jump_from(0)
+        return goal == 0
