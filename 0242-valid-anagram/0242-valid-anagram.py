d7593adff519fb246277024768575e338cc89cc1
HypothesisTester <130254113+HypothesisTester@users.noreply.github.com>
2026-02-18 00:34:13 +0000

    Time: 11 ms (75.89%), Space: 19.4 MB (68.96%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,15 +1,15 @@
+from collections import defaultdict
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-
-        countS, countT = {}, {}
+        
+        countS, countT = defaultdict(int), defaultdict(int)
 
         for i in range(len(s)):
-            # get current count or 0 if missing, then add 1
-            countS[s[i]] = 1 + countS.get(s[i], 0)
-            countT[t[i]] = 1 + countT.get(t[i], 0)
+            countS[s[i]] += 1
+            countT[t[i]] += 1
+        
         return countS == countT
-
-# Time: O(n)
-# Space: O(1)  (at most 26 letters)
+        
