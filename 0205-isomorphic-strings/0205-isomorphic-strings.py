6bfb80ad8f2cf863f83252a741e0c1dae56f6ebb
HypothesisTester <130254113+HypothesisTester@users.noreply.github.com>
2026-03-01 22:26:30 +0000

    Time: 3 ms (91.7%), Space: 19.1 MB (89.12%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,23 +1,7 @@
-from collections import defaultdict
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
+        # If they are isomorphic, the number of unique characters in s, 
+        # the number of unique characters in t, and the number of 
+        # unique PAIRS of characters must all be exactly the same.
         
-        map_s = defaultdict(list)
-        map_t = defaultdict(list)
-        
-        for i in range(len(s)):
-            char_s = s[i]
-            char_t = t[i ] 
-
-            if char_s in map_s and map_s[char_s] != char_t:
-                return False
-
-            if char_t in map_t and map_t[char_t] != char_s:
-                return False
-
-            map_s[char_s] = char_t
-            map_t[char_t] = char_s
-
-        return True
+        return len(set(s)) == len(set(t)) == len(set(zip(s, t)))
