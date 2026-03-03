c9a903f5b5b02d80b3d96cc9af1bd9982b4961b8
HypothesisTester <130254113+HypothesisTester@users.noreply.github.com>
2026-03-03 11:35:21 +0000

    Time: 35 ms (84.06%), Space: 24.4 MB (20.44%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,34 +1,26 @@
-# Solution: DP (top-down with memoization)
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         m, n = len(word1), len(word2)
+        memo = {}
 
-        dp = [[float("inf")] * n for _ in range(m)]
+        def dfs(i,j):
+            if i == m: return n - j
+            if j == n: return m - i
 
-        def dfs(i, j):
-            # If word1 is exhausted, insert remaining word2
-            if i == m:
-                return n - j
-            # If word2 is exhausted, delete remaining word1
-            if j == n:
-                return m - i
-            
-            if dp[i][j] != float("inf"):
-                return dp[i][j]
-            
-            # If characters match, move to next
+            if (i, j) in memo:
+                return memo[(i, j)]
+
             if word1[i] == word2[j]:
-                dp[i][j] = dfs(i + 1, j + 1)
-            # Otherwise, try all operations
+                memo[(i, j)] = dfs(i + 1, j + 1)
             else:
-                insert = 1 + dfs(i, j + 1) 
-                delete = 1 + dfs(i + 1, j)
-                replace = 1 + dfs(i + 1, j + 1)
-
-                dp[i][j] = min(insert, delete, replace)
-            return dp[i][j]
+                    memo[(i, j)] = 1 + min(
+                    dfs(i, j + 1),    
+                    dfs(i + 1, j),    
+                    dfs(i + 1, j + 1) 
+                )
+                
+            return memo[(i, j)]
 
         return dfs(0, 0)
 
-# Time: O(m*n)
-# Space: O(m*n) for dp + O(m+n) recursion stack
+        
