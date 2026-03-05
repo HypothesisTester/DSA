087ed88fac663c87e6e0c6b5223d6182028e53ed
HypothesisTester <130254113+HypothesisTester@users.noreply.github.com>
2026-03-05 11:37:57 +0000

    Time: 23 ms (78.76%), Space: 33.5 MB (65.61%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,23 +1,16 @@
-# Solution: Fast & Slow Pointers
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         slow = nums[0]
         fast = nums[0]
-
-        # find intersection in the cycle
         while True:
             slow = nums[slow]
             fast = nums[nums[fast]]
             if slow == fast:
                 break
 
-        # find cycle entry (duplicate)
         slow2 = nums[0]
-        while slow != slow2:
-            slow = nums[slow]
+        while slow2 != slow:
             slow2 = nums[slow2]
-
+            slow = nums[slow]
         return slow
-
-# Time: O(n)
-# Space: O(1)
+        
