6a2f87d27ede55906d38e5a2d206e63d6eb66b00
HypothesisTester <130254113+HypothesisTester@users.noreply.github.com>
2026-03-09 21:49:22 +0000

    Time: 7 ms (93.73%), Space: 21.7 MB (60.86%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,17 +1,21 @@
-# Solution: Bottom up DP
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        ROWS, COLS = len(grid), len(grid[0])
+        rows = len(grid)
+        cols = len(grid[0])
 
-        res = [[float("inf")] * (COLS + 1) for r in range(ROWS + 1)]
-                # Adjust value or values adjacent to bottom-right cell (0 indexed)
-        res[ROWS - 1][COLS] = 0
-        res[ROWS][COLS - 1] = 0 
- 
-        for r in range( ROWS - 1, -1, -1):
-            for c in range(COLS - 1, -1, -1):
-                res[r][c] = grid[r][c] + min(res[r + 1][c], res[r][c + 1])
-        return res[0][0]
+        dp = [[0] * cols for _ in range(rows)]
 
-# Time: O(M*N), Where m is the number of rows and n is the number of columns.
-# Space: O(M*N)
+        dp[0][0] = grid[0][0]
+
+        for j in range(1, cols):
+            dp[0][j] = grid[0][j] + dp[0][j-1]
+
+        for i in range(1, rows):
+            dp[i][0] = grid[i][0] + dp[i-1][0]
+
+        for i in range(1, rows):
+            for j in range(1, cols):
+                dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1])
+        
+        return dp[rows-1][cols-1]
+        
