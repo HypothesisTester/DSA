47c1ecdcb47229dce105c0d7bb048cb0a9cdf150
HypothesisTester <130254113+HypothesisTester@users.noreply.github.com>
2026-03-10 09:52:22 +0000

    Time: 1 ms (39.08%), Space: 22.3 MB (55.7%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -4,22 +4,23 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-# Iterative Solution
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        n = 0
-        stack = []
-        cur = root
+        count = 0
+        result = 0
 
-        # inorder traversal until we find the k-th node
-        while cur or stack:
-            while cur:
-                stack.append(cur)     # go left as far as possible
-                cur = cur.left
+        def inorder(node):
+            nonlocal count, result
+            if node is None:
+                return
+
+            inorder(node.left)
+
+            count += 1
+            if count == k:
+                result = node.val
+                return
+            inorder(node.right)
             
-            cur = stack.pop()         # visit node
-            n += 1
-            if n == k:                # check if it's the k-th
-                return cur.val
-            cur = cur.right           # then go right
+        inorder(root)
+        return result
