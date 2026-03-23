418c0cc3cac970fb7b7af0b353819b26538c4001
HypothesisTester <130254113+HypothesisTester@users.noreply.github.com>
2026-03-23 10:57:52 +0000

    Time: 0 ms (100%), Space: 19.2 MB (94.41%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.py
+++ 0257-binary-tree-paths/0257-binary-tree-paths.py
@@ -10,15 +10,15 @@ class Solution:
             return []
         
         result = []
-        
+
         def dfs(node, path):
-            if not node.left and not node.right:  # leaf node
+            if not node.left and not node.right:
                 result.append(path)
                 return
             if node.left:
                 dfs(node.left, path + "->" + str(node.left.val))
             if node.right:
                 dfs(node.right, path + "->" + str(node.right.val))
-        
+
         dfs(root, str(root.val))
         return result
