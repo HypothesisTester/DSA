8fc8b42b53617e29c11addea4bb43759d96345ed
HypothesisTester <130254113+HypothesisTester@users.noreply.github.com>
2026-03-31 00:35:21 +0100

    Time: 0 ms (100%), Space: 19.4 MB (46.33%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -7,4 +7,8 @@ class Solution:
             k += 1
 
         return numof5
-        
+
+# num of zeros = num of times 10 divides
+# 10 can be factored into 2 * 5
+# Take minimum of  num of 2 and num of 5 and their powers in n
+# realise num of 5s will always be smaller so you can just find that
